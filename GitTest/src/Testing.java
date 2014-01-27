dfaae45867071b027d3d8a5148dd7ba6d8573f2a
siketh <troman360@gmail.com>
2014-01-27 14:11:03 -0500

    Update 1
    Testing to see how updates work.

--- GitTest/src/Testing.java
+++ GitTest/src/Testing.java
@@ -4,5 +4,6 @@ public class Testing
 	public static void main(String[] args) 
 	{
 		System.out.println("AmIdoinitright????");
+		System.out.println("Making a change to see how that works too....");
 	}
 }
