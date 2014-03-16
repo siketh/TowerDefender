dd482fa3c5c6916e3cfd9bc74fa8591d6a75d573
Bobby Hernandez <killerjaguar26@gmail.com>
2014-03-16 19:20:05 -0400

    Merged master

--- TowerDefense/src/com/group23/towerdefense/TowerDefense.java
+++ TowerDefense/src/com/group23/towerdefense/TowerDefense.java
@@ -7,47 +7,45 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.group23.towerdefense.enemy.Enemy;
-import com.group23.towerdefense.enemy.Enemy1;
-import com.group23.towerdefense.enemy.Enemy2;
 import com.group23.towerdefense.level.Level;
 import com.group23.towerdefense.tower.Tower;
 import com.group23.towerdefense.ui.TopBar;
 import com.group23.towerdefense.ui.TowerBar;
 
-public class TowerDefense extends Game 
+public class TowerDefense extends Game
 {
-	public final static int SCREEN_WIDTH  = 1920;
+	public final static int SCREEN_WIDTH = 1920;
 	public final static int SCREEN_HEIGHT = 1080;
 	public final static int TILE_SIZE = 128; // width and height of a tile
-	
+
 	public static ShapeRenderer shapeRenderer;
 	public static SpriteBatch spriteBatch;
-	
+
 	private static TowerDefense game;
-	
+
 	@Override
 	// Essentially just loads the game
-	public void create() 
+	public void create()
 	{
 		game = this;
-		
+
 		// set debug variables
-		Tower.DEBUG_DRAWRANGE  = true;
+		Tower.DEBUG_DRAWRANGE = true;
 		Tower.DEBUG_DRAWTARGET = true;
-		
+
 		// initialize textures and fonts
-		Enemy.font     = new BitmapFont();
+		Enemy.font = new BitmapFont();
 		TopBar.health_font = new BitmapFont();
 		TopBar.gold_font = new BitmapFont();
-		 
+
 		TopBar.texture = ResourceManager.loadTexture("button.png");
 		TopBar.start_b = ResourceManager.loadTexture("start_b.png");
 		TopBar.tower_b = ResourceManager.loadTexture("tower_b.png");
-		TopBar.health  = ResourceManager.loadTexture("health.png");
-		TopBar.gold    = ResourceManager.loadTexture("gold.png");
-		
+		TopBar.health = ResourceManager.loadTexture("health.png");
+		TopBar.gold = ResourceManager.loadTexture("gold.png");
+
 		Level.background = ResourceManager.loadTexture("background.png");
-		Level.textures    = new Texture[8];
+		Level.textures = new Texture[8];
 		Level.textures[0] = ResourceManager.loadTexture("tile00.png");
 		Level.textures[1] = ResourceManager.loadTexture("tile01.png");
 		Level.textures[2] = ResourceManager.loadTexture("tile02.png");
@@ -56,23 +54,23 @@ public class TowerDefense extends Game
 		Level.textures[5] = ResourceManager.loadTexture("tile05.png");
 		Level.textures[6] = ResourceManager.loadTexture("tile06.png");
 		Level.textures[7] = ResourceManager.loadTexture("tile07.png");
-		
+
 		TowerBar.textures = new Texture[2];
 		TowerBar.textures[0] = ResourceManager.loadTexture("towerbar00.png");
 		TowerBar.textures[1] = ResourceManager.loadTexture("towerbar01.png");
-		//TowerBar.textures[2] = ResourceManager.loadTexture("towerbar03.png");
-		
+		// TowerBar.textures[2] = ResourceManager.loadTexture("towerbar03.png");
+
 		// initialize static batches
-		spriteBatch   = new SpriteBatch();
+		spriteBatch = new SpriteBatch();
 		shapeRenderer = new ShapeRenderer();
-		
+
 		// set the screen to its initial state
 		setScreen(new GameplayScreen(1));
 	}
 
 	@Override
 	// Called when game is killed, unloads everything
-	public void dispose() 
+	public void dispose()
 	{
 		for (Texture t : Level.textures)
 			t.dispose();
@@ -82,7 +80,7 @@ public class TowerDefense extends Game
 		spriteBatch.dispose();
 		ResourceManager.dispose();
 	}
-	
+
 	static public void changeScreen(Screen screen)
 	{
 		game.setScreen(screen);
