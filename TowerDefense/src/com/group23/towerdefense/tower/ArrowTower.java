7347e9dd177ca37ae031350696133ac37beed814
Colegreen <ColeGarner@knights.ucf.edu>
2014-04-04 14:33:26 -0400

    unneccessary code

--- TowerDefense/src/com/group23/towerdefense/tower/ArrowTower.java
+++ TowerDefense/src/com/group23/towerdefense/tower/ArrowTower.java
@@ -1,9 +1,5 @@
 package com.group23.towerdefense.tower;
 
-import com.group23.towerdefense.enemy.Debuff;
-import com.group23.towerdefense.enemy.DebuffType;
-import com.group23.towerdefense.enemy.Enemy;
-
 public class ArrowTower extends SingleTargetCircularRangeTower
 {
 	public ArrowTower()
