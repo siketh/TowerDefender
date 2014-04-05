8626e469da76f23714bb514a570d213e2a0700c7
BlaineDeschain <jacob.k@knights.ucf.edu>
2014-04-05 02:30:42 -0400

    test

--- TowerDefense/src/com/group23/towerdefense/screen/LevelSelectScreen.java
+++ TowerDefense/src/com/group23/towerdefense/screen/LevelSelectScreen.java
@@ -1,14 +1,18 @@
 package com.group23.towerdefense.screen;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.group23.towerdefense.DefaultLevelGenerator;
 import com.group23.towerdefense.Level;
 import com.group23.towerdefense.TowerDefense;
+import com.badlogic.gdx.files.FileHandle;
+
 
 public class LevelSelectScreen extends BaseScreen
 {
 	private Level.Generator generator = new DefaultLevelGenerator();
+	private FileHandle handle = Gdx.files.local("data/user-progress.xml");
 
 	@Override
 	public void show()
