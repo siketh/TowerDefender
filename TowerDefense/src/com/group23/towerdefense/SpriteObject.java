7c90d3874b7157a9f8cb70fcc4f8b6211fe160c4
Colegreen <ColeGarner@knights.ucf.edu>
2014-04-06 15:00:24 -0400

    Created Sprite Object for projectiles

---
+++ TowerDefense/src/com/group23/towerdefense/SpriteObject.java
@@ -0,0 +1,37 @@
+package com.group23.towerdefense;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+
+public class SpriteObject extends TextureObject
+{
+	private Sprite mSprite;
+	
+	public void setTexture(String filename)
+	{
+		super.setTexture(filename);
+		mSprite = new Sprite(getTexture());
+	}
+	
+	public void setTexturePosition(float x, float y)
+	{
+		mSprite.setPosition(x, y);
+	}
+	
+	public void setTexturePosition(Vector2 pos)
+	{
+		mSprite.setPosition(pos.x, pos.y);
+	}
+	
+	public Vector2 getTexturePosition()
+	{
+		return new Vector2(mSprite.getX(), mSprite.getY());
+	}
+	
+	public void draw(Batch batch)
+	{
+		if (mSprite != null)
+			mSprite.draw(batch);
+	}
+}
